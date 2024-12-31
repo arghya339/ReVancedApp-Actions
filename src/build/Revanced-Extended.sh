e7291e2db34f6d63e796353736a1a8670359a2f7
arghya339 <arghyadeepmondal339@duck.com>
2024-12-31 21:56:42 +0530

    drop YT Music RVX 7.16.53 https://github.com/inotia00/ReVanced_Extended/issues/2382

--- src/build/Revanced-Extended.sh
+++ src/build/Revanced-Extended.sh
@@ -40,18 +40,22 @@ source src/build/utils.sh
 	# Patch YouTube Music Extended:
 	# Arm64-v8a
 	get_patches_key "youtube-music-revanced-extended"
+	version="7.16.53"
 	get_apk "com.google.android.apps.youtube.music" "youtube-music-arm64-v8a" "youtube-music" "google-inc/youtube-music/youtube-music" "arm64-v8a"
 	patch "youtube-music-arm64-v8a" "revanced-extended" "inotia"
 	# Armeabi-v7a
 	get_patches_key "youtube-music-revanced-extended"
+	version="7.16.53"
 	get_apk "com.google.android.apps.youtube.music" "youtube-music-armeabi-v7a" "youtube-music" "google-inc/youtube-music/youtube-music" "armeabi-v7a"
 	patch "youtube-music-armeabi-v7a" "revanced-extended" "inotia"
 	# x86
 	get_patches_key "youtube-music-revanced-extended"
+	version="7.16.53"
 	get_apk "com.google.android.apps.youtube.music" "youtube-music-x86" "youtube-music" "google-inc/youtube-music/youtube-music" "x86"
 	patch "youtube-music-x86" "revanced-extended" "inotia"
 	# x86_64
 	get_patches_key "youtube-music-revanced-extended"
+	version="7.16.53"
 	get_apk "com.google.android.apps.youtube.music" "youtube-music-x86_64" "youtube-music" "google-inc/youtube-music/youtube-music" "x86_64"
 	patch "youtube-music-x86_64" "revanced-extended" "inotia"	
 	
